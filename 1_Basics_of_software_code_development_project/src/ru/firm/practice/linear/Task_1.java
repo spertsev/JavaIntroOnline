f642e63d39f8c64ea98b583cae064d3939589841
Sergey Pertsev <s.pertsev@outlook.com>
2019-10-23 00:38:33 +0300

    Updated class '1_Basics_of_software_code_development_project/src/ru/firm/practice/linear/Task_1.java'

--- 1_Basics_of_software_code_development_project/src/ru/firm/practice/linear/Task_1.java
+++ 1_Basics_of_software_code_development_project/src/ru/firm/practice/linear/Task_1.java
@@ -1,10 +1,29 @@
 package ru.firm.practice.linear;
 
+import java.util.Scanner;
+
 public class Task_1 {
+	
+	
 
 	public static void main(String[] args) {
-		System.out.println("Hi world");
+		
+		float a, b, c, z;
+		
+		Scanner scannerObject = new Scanner(System.in);
+		
+		System.out.println("Calculating a result of function 'z = ((a - 3) * b / 2) + c'");
+		
+		System.out.print("Input a float value of 'a':");
+		a = scannerObject.nextFloat();
+		System.out.print("Input a float value of 'b':");
+		b = scannerObject.nextFloat();
+		System.out.print("Input a float value of 'c':");
+		c = scannerObject.nextFloat();
+		
+		z = ((a - 3) * b / 2) + c;
+				
+		System.out.println("z=" + z);
 
 	}
-
 }
