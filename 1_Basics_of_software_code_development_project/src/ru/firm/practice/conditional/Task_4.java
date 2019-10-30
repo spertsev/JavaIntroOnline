04cb210208976318891acdad0a4f881ff2c03025
Sergey Pertsev <s.pertsev@outlook.com>
2019-10-31 00:14:19 +0300

    Added class 'practice/conditional/Task_4.java'

---
+++ 1_Basics_of_software_code_development_project/src/ru/firm/practice/conditional/Task_4.java
@@ -0,0 +1,32 @@
+package ru.firm.practice.conditional;
+
+import java.util.Scanner;
+
+public class Task_4 {
+
+	public static void main(String[] args) {
+
+		int A, B, x, y, z;
+
+		Scanner scannerObject = new Scanner(System.in);
+
+		System.out.print("Input length of rectangle's side 'A':");
+		A = scannerObject.nextInt();
+		System.out.print("Input length of rectangle's side 'B':");
+		B = scannerObject.nextInt();
+		System.out.print("Input the dimension 'x' of the brick:");
+		x = scannerObject.nextInt();
+		System.out.print("Input the dimension 'y' of the brick:");
+		y = scannerObject.nextInt();
+		System.out.print("Input the dimension 'z' of the brick:");
+		z = scannerObject.nextInt();
+
+		scannerObject.close();
+
+		if ((x < A && y < B) || (x < A && z < B) || (y < A && z < B))
+			System.out.print("True. The brick goes through the hole");
+		else
+			System.out.print("False. The brick is too big");
+
+	}
+}
