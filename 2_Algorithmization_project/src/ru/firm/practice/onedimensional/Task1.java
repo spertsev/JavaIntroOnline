045060f62a9dc417c7a21477c6cac78a7e17e6f0
Sergey Pertsev <s.pertsev@outlook.com>
2019-11-20 21:06:19 +0300

    modified:   2_Algorithmization_project/src/ru/firm/practice/onedimensional/Task1.java

--- 2_Algorithmization_project/src/ru/firm/practice/onedimensional/Task1.java
+++ 2_Algorithmization_project/src/ru/firm/practice/onedimensional/Task1.java
@@ -1,9 +1,40 @@
 package ru.firm.practice.onedimensional;
 
+/*
+ * 1. В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+ */
+
+
+import java.util.Scanner;
+
 public class Task1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int arrayLength = 0, i, k, sum = 0;
+		int[] arr;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input value of 'N' (a length of the naural numbers array):");
+		arrayLength = sc.nextInt();
+		System.out.print("Input value of 'K':");
+		k = sc.nextInt();
+		sc.close();
+
+		System.out.print("The array members which are multiple to the " + k + " are: ");
+
+		arr = new int[arrayLength];
+
+		for (i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+			if (arr[i] % k == 0) {
+				System.out.print(arr[i] + " ");
+				sum = sum + arr[i];
+			}
+
+		}
+		
+		System.out.print("\nSum of the array members, which are multiple to the 2, is: " + sum);
 
 	}
 
