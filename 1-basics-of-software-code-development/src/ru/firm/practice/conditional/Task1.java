33698dfc89bc9ac5c15a8fdd493d1d73de00636a
spertsev <s.pertsev@outlook.com>
2020-03-14 23:59:13 +0300

    Modified class 1-basics-of-software-code-development/src/ru/firm/practice/conditional/Task1.java

--- 1-basics-of-software-code-development/src/ru/firm/practice/conditional/Task1.java
+++ 1-basics-of-software-code-development/src/ru/firm/practice/conditional/Task1.java
@@ -2,36 +2,49 @@ package ru.firm.practice.conditional;
 
 import java.util.Scanner;
 
+/*
+ *  1. Даны два угла треугольника (в градусах). Определить, существует ли такой треугольник, и если да, то будет ли он прямоугольным.
+ */
+
 public class Task1 {
 
 	public static void main(String[] args) {
 
-		int angle1, angle2;
-		boolean triangleExists, isRectangular;
-
-		Scanner scannerObject = new Scanner(System.in);
+		int firstAngle;
+		int secondAngle;
+		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Input values of the triangle's two angles");
+
 		System.out.print("The first angle:");
-		angle1 = scannerObject.nextInt();
+		firstAngle = typeIntegerValue(scanner);
+
 		System.out.print("The second angle:");
-		angle2 = scannerObject.nextInt();
-		scannerObject.close();
+		secondAngle = typeIntegerValue(scanner);
 
-		if (angle1 > 0 && angle2 > 0 && (angle1 + angle2) < 180)
-			triangleExists = true;
-		else
-			triangleExists = false;
-		System.out.println("Does the triangle exist?: " + triangleExists);
-
-		if (!triangleExists)
+		if (firstAngle > 0 && secondAngle > 0 && (firstAngle + secondAngle) < 180) {
+			System.out.println("The triangle exists!");
+		} else {
+			System.out.println("The triangle doesn't exist!");
 			return;
+		}
 
-		if (angle1 == 90 || angle2 == 90 || (angle1 + angle2) == 90)
-			isRectangular = true;
-		else
-			isRectangular = false;
-		System.out.print("Is the triangle rectangular?: " + isRectangular);
+		if (firstAngle == 90 || secondAngle == 90 || (firstAngle + secondAngle) == 90) {
+			System.out.print("The triangle is rectangular!");
+		} else {
+			System.out.print("The triangle is not rectangular!");
+		}
 
 	}
+
+	public static int typeIntegerValue(Scanner scanner) {
+		while (!scanner.hasNextInt()) {
+			System.out.print("You've typed a not integer value!");
+			scanner.next();
+			System.out.print("\nPlease input an integer:");
+		}
+		
+		return scanner.nextInt();
+	}
+
 }
