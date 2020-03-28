db53121ed966d1ae71e0b3ebbbd383c821ba5180
Sergey Pertsev <s.pertsev@outlook.com>
2020-03-28 21:53:23 +0300

    Added class 2-algorithmization/src/ru/firm/practice/onedimensional/Task3.java

---
+++ 2-algorithmization/src/ru/firm/practice/onedimensional/Task3.java
@@ -0,0 +1,30 @@
+package ru.firm.practice.onedimensional;
+
+/**
+ * 3.Дан массив действительных чисел, размерность которого N. Подсчитать,
+ * сколько в нем отрицательных, положительных и нулевых элементов.
+ */
+public class Task3 {
+
+	public static void main(String[] args) {
+		double[] realNumbers = { 2, 3, 0, -1, -5, 0, 0, -7 };
+		int positiveCount = 0;
+		int negativeCount = 0;
+		int zerosCount = 0;
+
+		for (double currentNumber : realNumbers) {
+			if (currentNumber > 0) {
+				positiveCount++;
+			} else if (currentNumber < 0) {
+				negativeCount++;
+			} else {
+				zerosCount++;
+			}
+		}
+
+		System.out.println("Positive elements count: " + positiveCount);
+		System.out.println("Negative elements count: " + negativeCount);
+		System.out.println("Zero elements count: " + zerosCount);
+	}
+
+}
